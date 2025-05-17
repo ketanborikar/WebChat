33f6d7919ce796a8fcabcdbb5bb2a65edee3dcea
Ketan <ketan.borikar@gmail.com>
2025-05-18 02:08:22 +0530

    Folder correction

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-import psycopg2
-
-load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-conn = psycopg2.connect(DATABASE_URL)
-cursor = conn.cursor()
