ee488531e92b4ebb17a50ac6e5fd1d01e177eb02
Ketan <ketan.borikar@gmail.com>
2025-05-18 03:06:33 +0530

    Folder correction

---
+++ script.js
@@ -0,0 +1,42 @@
+const socket = io("https://webchat-yoaw.onrender.com");
+
+function signup() {
+    let username = document.getElementById("signup-username").value;
+    let password = document.getElementById("signup-password").value;
+
+    fetch("https://webchat-yoaw.onrender.com/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password })
+    }).then(response => response.json())
+      .then(data => alert(data.message));
+}
+
+function login() {
+    let username = document.getElementById("login-username").value;
+    let password = document.getElementById("login-password").value;
+
+    fetch("https://webchat-yoaw.onrender.com/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password })
+    }).then(response => response.json())
+      .then(data => {
+          if (data.access_token) {
+              localStorage.setItem("token", data.access_token);
+              alert("Login Successful!");
+          } else {
+              alert("Invalid Credentials");
+          }
+      });
+}
+
+socket.on("message", function(data) {
+    let chatBox = document.getElementById("chat-box");
+    chatBox.innerHTML += `<p><strong>${data.sender}:</strong> ${data.content}</p>`;
+});
+
+function sendGroupMessage() {
+    let content = document.getElementById("chat-message").value;
+    socket.emit("message", { sender: "User", group: "main", content });
+}
