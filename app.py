29c1aa9df6f7c0f1fee7742e23f15a11bc048cc8
Ketan <ketan.borikar@gmail.com>
2025-05-18 02:43:23 +0530

    Folder correction

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_socketio import SocketIO
+import eventlet
+import eventlet.wsgi
 from auth import auth_bp
 from chat import chat_bp, socketio
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['JWT_SECRET_KEY'] = 'your_jwt_secret'
+app.config['SECRET_KEY'] = '7e1c7112fe937fe24e8fe1dc84d56299'
+app.config['JWT_SECRET_KEY'] = 'c9de8c1fd839edef2260ab6f27f9dd1c'
 
 jwt = JWTManager(app)
 
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(chat_bp, url_prefix='/chat')
 
+# Explicitly using eventlet async mode
+socketio.init_app(app, async_mode='eventlet')
+
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
